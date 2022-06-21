16c2906a3411340b8e18482838bcc2dda19629cf
ViktorHefringMarine <viktor@hefringmarine.com>
2022-06-21 14:33:26 +0000

    rust windows

--- lua/config/lsp/rust_lsp.lua
+++ lua/config/lsp/rust_lsp.lua
@@ -352,10 +352,10 @@ cmp.setup({
         -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-y>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-e>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
+        -- ['<C-q'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
